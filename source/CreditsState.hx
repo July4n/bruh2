01321b35fcdf9024fa649c03a47d7106aff918c0
Julyan <95003100+July4n@users.noreply.github.com>
2022-10-03 13:25:52 -0500

    Toque para editar texto

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -81,10 +81,10 @@ class CreditsState extends MusicBeatState
 		#end
 
 		var pisspoop:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color
-                        ['Psych Engine Android Port'],
-                        ['idklool (or Boyfriend FNF idc)', 'nothing', 'Hi I ported the Psych Engine', 'youtube.com/c/BoyfriendFNF', 'FFFFFF'],
-                        ['Dxgamer', 'dxgamer', 'Coder Help', 'https://youtube.com/c/Dxgamer7405', 'FFFFFF'],
-			['Psych Engine Team'],
+                        ['Android Port'],
+			['Julyan',	'Julyan',				'Yo Soy el femboy engine',	'https://youtube.com/channel/UCNAe-mX-I647TGBFs4dhUmw', 	'FFCDCDCD'],
+			[''],
+                        ['Psych Engine Team'],
 			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',								'https://twitter.com/Shadow_Mario_',	'444444'],
 			['RiverOaken',			'river',			'Main Artist/Animator of Psych Engine',							'https://twitter.com/RiverOaken',		'B42F71'],
 			['shubs',				'shubs',			'Additional Programmer of Psych Engine',						'https://twitter.com/yoshubs',			'5E99DF'],
