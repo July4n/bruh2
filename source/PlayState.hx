4c578a53a7fbcc9363cbf38ffce8814d1696e8c9
Julyan <95003100+July4n@users.noreply.github.com>
2022-10-03 01:01:56 -0500

    Si

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2264,10 +2264,8 @@ class PlayState extends MusicBeatState
 	public function updateScore(miss:Bool = false)
 	{
 		scoreTxt.text = 'Score: ' + songScore
-		+ ' | Misses: ' + songMisses
-		+ ' | Rating: ' + ratingName
-		+ (ratingName != '?' ? ' (${Highscore.floorDecimal(ratingPercent * 100, 2)}%) - $ratingFC' : '');
-
+		+ ' | Combo Breaks: ' + songMisses
+		+ ' | Accuracy: ' + Highscore.floorDecimal(ratingPercent * 100, 2) + '%' + '[' + ratingFC+ ']';
 		if(ClientPrefs.scoreZoom && !miss && !cpuControlled)
 		{
 			if(scoreTxtTween != null) {
