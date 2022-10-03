46b99e9c80162ee0cb15e5d1067edd2abe70ae8b
Julyan <95003100+July4n@users.noreply.github.com>
2022-10-03 14:22:47 -0500

    Presion

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -19,6 +19,7 @@ class FlashingState extends MusicBeatState
 	public static var leftState:Bool = false;
 
 	var warnText:FlxText;
+
 	override function create()
 	{
 		super.create();
@@ -26,27 +27,17 @@ class FlashingState extends MusicBeatState
 		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
 		add(bg);
 
-		#if android
 		warnText = new FlxText(0, 0, FlxG.width,
-			"Esta Verga tiene Luces
-                        Presiona "A" Si Te Vale Verga!",
+			"esto es muy Bizarro Y tiene Luces 
+                         PRESIONA A SI TE VALE VERGA",
 			32);
-		#else
-		warnText = new FlxText(0, 0, FlxG.width,
-			"Hey, watch out!\n
-			This Mod contains some flashing lights!\n
-			Press ENTER to disable them now or go to Options Menu.\n
-			Press ESCAPE to ignore this message.\n
-			You've been warned!",
-			32);
-		#end
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
 		add(warnText);
 
-		#if android
-		addVirtualPad(NONE, A_B);
-		#end
+                #if android
+                addVirtualPad(NONE, A_B);
+                #end
 	}
 
 	override function update(elapsed:Float)
@@ -62,18 +53,12 @@ class FlashingState extends MusicBeatState
 					ClientPrefs.saveSettings();
 					FlxG.sound.play(Paths.sound('confirmMenu'));
 					FlxFlicker.flicker(warnText, 1, 0.1, false, true, function(flk:FlxFlicker) {
-						#if android
-						virtualPad.alpha = 0;
-						#end
 						new FlxTimer().start(0.5, function (tmr:FlxTimer) {
 							MusicBeatState.switchState(new TitleState());
 						});
 					});
 				} else {
 					FlxG.sound.play(Paths.sound('cancelMenu'));
-					#if android
-					FlxTween.tween(virtualPad, {alpha: 0}, 1);
-					#end
 					FlxTween.tween(warnText, {alpha: 0}, 1, {
 						onComplete: function (twn:FlxTween) {
 							MusicBeatState.switchState(new TitleState());
